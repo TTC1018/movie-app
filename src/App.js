42f347f8dc90712af088f3d7ff735455bf31c79d
ttc1018 <emforhs246@naver.com>
2019-11-29 21:28:40 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Movie title = {movieTitles[0]} poster = {movieImages[0]}/>     
       <Movie title = {movieTitles[1]} poster = {movieImages[1]}/> 
       <Movie title = {movieTitles[2]} poster = {movieImages[2]}/> 
-      <Movie title = {movieTitles[3]} poster = {movieImaposter[3]}/>  
+      <Movie title = {movieTitles[3]} poster = {movieImages[3]}/>  
     </div>
   );
 }

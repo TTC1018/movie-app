22e20e8be4692a2197e976dfebdfdeaf7ec6bfe7
ttc1018 <emforhs246@naver.com>
2019-11-29 21:51:02 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movieTitles = [
-  "Starwars",
-  "Memento",
-  "Batman",
-  "The Avengers"
-]
 
-const movieImages = [
-  "https://media.comicbook.com/2019/10/star-wars-the-rise-of-skywalker-final-trailer-1192360-1280x0.jpeg",
-  "https://www.kanopy.com/sites/default/files/imagecache/vp_thumbnail_extremly_large/video-assets/2485021_landscape.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/91KkWf50SoL._SL1500_.jpg",
-  "https://movietheaterprices.com/wp-content/uploads/2018/10/The-Avengers-4-1024x506.png"
+const movies = [
+  {
+  title : "Starwars",
+  poster : "https://media.comicbook.com/2019/10/star-wars-the-rise-of-skywalker-final-trailer-1192360-1280x0.jpeg"
+  },
+  {
+  title : "Memento",
+  poster : "https://www.kanopy.com/sites/default/files/imagecache/vp_thumbnail_extremly_large/video-assets/2485021_landscape.jpg"
+  },
+  {
+  title : "Batman",
+  poster : "https://images-na.ssl-images-amazon.com/images/I/91KkWf50SoL._SL1500_.jpg"
+  },
+  {
+  title : "The Avengers",
+  poster : "https://movietheaterprices.com/wp-content/uploads/2018/10/The-Avengers-4-1024x506.png"
+  }
 ]
 
 function App() {
   return (
     <div className="App">
-      <Movie title = {movieTitles[0]} poster = {movieImages[0]}/>     
-      <Movie title = {movieTitles[1]} poster = {movieImages[1]}/> 
-      <Movie title = {movieTitles[2]} poster = {movieImages[2]}/> 
-      <Movie title = {movieTitles[3]} poster = {movieImages[3]}/>  
+      {movies.map(movie => {
+        <Movie title = {movie.title} poster = {movie.poster} />
+      })} 
     </div>
   );
 }

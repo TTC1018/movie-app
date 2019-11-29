ff4cee89a538fac7442ee308a3368d7d22ed0fd6
ttc1018 <emforhs246@naver.com>
2019-11-29 21:17:02 +0900

    modified:   src/App.js 	modified:   src/Movie.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
 
+const movies = [
+  "Starwars",
+  "Memento",
+  "Batman",
+  "The Avengers"
+]
 
 function App() {
   return (
     <div className="App">
-      <Movie />      
+      <Movie title = {movies[0]}/>     
+      <Movie title = {movies[1]}/> 
+      <Movie title = {movies[2]}/> 
+      <Movie title = {movies[3]}/>  
     </div>
   );
 }
